887b01d31eb968c3c6e4aa1ab64703c58a075650
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-05-28 16:16:01 -0300

    create inde.js

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const restify = require('restify')
+
+const server = restify.createServer()
+
+server.get('/', (req, res, next) =>{
+
+	res.send('Enjoy the silence!')
+	
+	next()
+})
+
+server.listen('3456')
