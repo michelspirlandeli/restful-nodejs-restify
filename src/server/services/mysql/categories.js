10e397412bc52e57f3e6dae98dda0c1d8a03fa1c
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-06-01 16:42:37 -0300

    create all, save, update, del

---
+++ src/server/services/mysql/categories.js
@@ -0,0 +1,36 @@
+const categories = deps => {
+
+
+	return {
+
+		all: () => {
+
+			return new Promise((resolve, reject) => {
+
+			const { connection } = deps
+
+			connection.query('SELECT * FROM categories', (error, results) => {
+
+				if(error) {
+
+					reject(error)
+
+				}
+
+					resolve({ categories: results }) 
+				})
+
+			})
+			
+		},
+
+		save: (name) => {},
+
+		update: (id, name) => {},
+
+		del: (id) => {}
+	}
+
+}
+
+module.exports = categories
