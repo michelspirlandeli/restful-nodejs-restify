72cfecd80be5a8d5583fba18b5f70f021e727121
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-06-05 09:49:17 -0300

    update folders

---
+++ src/services/mysql/index.js
@@ -0,0 +1,29 @@
+const mysqlServer = require('mysql')
+
+const connection = mysqlServer.createConnection({
+
+	host: process.env.MYSQL_HOST,
+
+	user: process.env.MYSQL_USER,
+
+	password: process.env.MYSQL_PASSWORD,
+
+	database: process.env.MYSQL_DATABASE
+
+})
+
+const errorHandler = (error, msg, rejectFunction) => {
+
+	console.error(error)
+
+	rejectFunction({ error: msg })
+}
+
+const categoryModule = require('./categories')({ connection, errorHandler })
+
+module.exports = {
+
+	categories: () => categoryModule
+
+
+}
