e1e962549aa2a72a2b65fe230ee3ec15a8f328dd
michel spirlandeli <michelspirlandeli10@gmail.com>
2018-06-08 14:36:32 -0300

    update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,18 @@ const server = restify.createServer()
 
 const routes = require('../http/routes')
 
+const cors = require('./cors')
+
+const jwtMiddleware = require('./jwtMiddleware')
+
+const exclusions = ['/autenticacao']
+
+server.pre(cors.preflight)
+server.use(cors.actual)
+server.use(restify.plugins.bodyParser())
+
+server.use(jwtMiddleware({ exclusions }))
+
 routes(server)
 
 module.exports = server
